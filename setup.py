13430e70bfd01afce068e32794e900a230ea8252
Nikolaos Coutsos <ncoutsos@synacor.com>
2015-03-08 15:58:20 -0400

    Add `clients` subpackage.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="cors",
     version="0.1.0",
     description="Utilities for creating CORS-enabled HTTP interfaces",
     author="Nick Coutsos",
-    email="ncoutsos@synacor.com",
-    packages=["cors"]
+    author_email="ncoutsos@synacor.com",
+    packages=find_packages(exclude=["*tests*"]),
 )
